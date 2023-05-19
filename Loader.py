b768815dcf872f9be52cccba80be0dfad564f190
alpertunga-bile <kingdom19online@hotmail.com>
2023-05-19 14:36:29 +0300

    New iterable objects are supported in Loader class

--- Loader.py
+++ Loader.py
@@ -1,4 +1,5 @@
 import sys
+from collections.abc import KeysView, ItemsView
 
 class Loader:
     desc = ""
@@ -7,24 +8,50 @@ class Loader:
     step = 1
     counter = 0
     loadChar = '#'
+    toAdd = 0
+    iteration = None
 
-    def __init__(self, range : range, description="Loader", loadingChar="#"):
-        if range.start < 0:
-            oneMinusMulStart = -1 * range.start
-            self.start = 0
-            self.end = range.stop + oneMinusMulStart
+    def SetValuesForRangeObject(self, givenRange : range, description : str, loadingChar : str):
+        if givenRange.start < 0 and givenRange.stop > 0:
+            self.toAdd = -1 * givenRange.start
+        elif givenRange.start < 0 and givenRange.stop < 0:
+            self.toAdd = -1 * min(givenRange.start, givenRange.stop)
+    
+        self.iteration = iter(givenRange)
+        self.SetAttributes(givenRange.start, givenRange.stop, givenRange.step, description, loadingChar)
+
+    def SetValuesForListTupleSetObjects(self, givenList : list, description : str, loadingChar : str):
+        self.iteration = iter(givenList)
+        self.SetAttributes(0, len(givenList), 1, description, loadingChar)
+
+    def SetValuesForDictKeysObject(self, givenDictKeys, description : str, loadingChar : str):
+        tempList = list(givenDictKeys)
+
+    def __init__(self, iterableObject, description="Loader", loadingChar="#"):
+        objectType = type(iterableObject)
+        
+        if objectType is range:
+            self.SetValuesForRangeObject(iterableObject, description, loadingChar)
+        elif objectType is list or objectType is tuple or objectType is set:
+            self.SetValuesForListTupleSetObjects(iterableObject, description, loadingChar)
+        elif isinstance(iterableObject, KeysView) or isinstance(iterableObject, ItemsView):
+            tempList = list(iterableObject)
+            self.SetValuesForListTupleSetObjects(tempList, description, loadingChar)
         else:
-            self.start = range.start
-            self.end = range.stop
+            print(f"{objectType} for iterableObjects is not supported")
+            exit(0)
 
-        self.step = range.step
-        self.counter = range.start
+    def SetAttributes(self, start : int, end : int, step : int, description : str, loadingChar : str):
+        self.start = start
+        self.end = end - 1
+        self.step = step
+        self.counter = start
         self.desc = description
         self.loadChar = loadingChar
 
     def __iter__(self):
-        self.counter = self.start
         self.Update()
+        self.counter = self.start - 1
         return self
     
     def RemoveLastLine(self):
@@ -32,16 +59,16 @@ class Loader:
         sys.stdout.write('\x1b[2K')
     
     def __next__(self):
+        self.counter = self.counter + self.step
         if self.counter <= self.end:
             self.RemoveLastLine()
             self.Update()
-            self.counter = self.counter + self.step
-            return self.counter
+            return next(self.iteration)
         else:
             raise StopIteration
 
     def Update(self):
-        percentage = int((float(self.counter) / float(self.end)) * 100.0)
+        percentage = int((float(self.counter + self.toAdd) / float(self.end + self.toAdd)) * 100.0)
         loadStr = self.desc + " ["
         loadStr += self.loadChar * percentage
         loadStr += " " * (100 - percentage)
--- main.py
+++ main.py
@@ -3,14 +3,11 @@ from VenvManager import VenvManager
 from Completer import Completer
 
 if __name__ == "__main__":
-    venvManager = VenvManager("venv")
+    file = open("test.txt", "w")
 
-    completer = Completer()
-    completeName = "yesno"
-    completeVocabs = ["yes", "no"]
-    completer.CreateCompleteFunction(completeName, completeVocabs)
-    completer.CreateCurrentDirectoryFilesCompleteFunction("currentDir")
-    completer.SetCompleteFunction(completeName)
-    test = input("Enter something : ")
-    completer.SetCompleteFunction("currentDir")
-    test = input("Enter something : ")
+    randomDict = {'Physics':67, 'Maths':87, 'Wololo':234}
+    
+    for key, value in Loader(randomDict.items(), "Test"):
+        file.write(f"{key} ==> {value}\n")
+
+    file.close()
