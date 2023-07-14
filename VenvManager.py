b9c09f08c6bd2db9bbd43a56ca8919c453ea2e12
alpertunga-bile <76731692+alpertunga-bile@users.noreply.github.com>
2023-07-14 12:14:13 +0000

    Add files via upload

--- VenvManager.py
+++ VenvManager.py
@@ -1,246 +1,306 @@
-from shutil import rmtree
-from os import remove
-from subprocess import call, STDOUT
-from os.path import exists
+from subprocess import run
 from platform import system
-from time import sleep
+from sys import stdout
+from os import remove, mkdir, getcwd
+from re import compile
+from os.path import join, exists
+from logging import getLogger, Logger, StreamHandler, Formatter, INFO
+from json import load, dumps
+
+# For coloring log outputs
+class CustomFormatter(Formatter):
+    grey = '\x1b[38;21m'
+    blue = '\x1b[38;5;39m'
+    yellow = '\x1b[38;5;226m'
+    red = '\x1b[38;5;196m'
+    bold_red = '\x1b[31;1m'
+    reset = '\x1b[0m'
+
+    def __init__(self, fmt):
+        from logging import DEBUG, WARNING, ERROR, CRITICAL
+        super().__init__()
+        self.fmt = fmt
+        self.FORMATS = {
+            DEBUG: self.grey + self.fmt + self.reset,
+            INFO: self.blue + self.fmt + self.reset,
+            WARNING: self.yellow + self.fmt + self.reset,
+            ERROR: self.red + self.fmt + self.reset,
+            CRITICAL: self.bold_red + self.fmt + self.reset
+        }
+
+    def format(self, record):
+        logFormat = self.FORMATS.get(record.levelno)
+        formatter = Formatter(logFormat)
+        return formatter.format(record)
 
 class VenvManager:
-    osName = ""
-    venvName = "venv"
-    isSilent = False
+    opsys : str = ""
+    venvName : str = ""
+    currentDir : str = ""
+    paths : dict = {}
+    logger : Logger
 
-    def __init__(self, envName, isSilent=False):
-        self.osName = system()
+    def __init__(self, envName : str ="venv", loggerLevel=INFO):
         self.venvName = envName
-        self.isSilent = isSilent
+        self.opsys = system()
+        self.paths = {}
+        self.currentDir = getcwd()
+        self.SetLogger(loggerLevel)
+        self.SetPaths(envName)
+        self.SavePaths()
 
-    def RunCommand(self, command):
-        if self.isSilent:
-            logFile = open("logs.txt", mode="w")
-            result = call(command, shell=True, stdout=logFile, stderr=STDOUT)
-            logFile.close()
+        self.logger.debug(self.paths)
+
+        file = open("venvLog.txt", "w")
+        file.close()
+        self.logger.info("VenvLog file is created")
+
+        # Check python and its version
+        self.CheckPython()
+
+        if exists(envName):
+            self.logger.info(f"Using {envName} virtual environment")
         else:
-            result = call(command, shell=True)
-            _ = print("Command executed successfully") if result == 0 else print(f"Error occured when running {command}")
+            self.logger.info(f"Creating {envName}")
+            self.CheckCommand(self.RunCommand("python -m venv venv"), f"{envName} is created", "Cant create virtual environment")
 
-    def IsEnvironmentCreated(self):
+    """
+    Class functionalities
+    """
+
+    def SetLogger(self, loggerLevel) -> None:
+        self.logger = getLogger()
+        self.logger.setLevel(loggerLevel)
+        handler = StreamHandler(stdout)
+        handler.setLevel(INFO)
+        logFormat = '[%(asctime)s] /_\ %(levelname)-8s /_\ %(message)s'
+        if self.opsys == "Windows":
+            formatter = Formatter(logFormat)
+        else:
+            formatter = CustomFormatter(logFormat)
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def IsEnvironmentExists(self) -> bool:
         return exists(self.venvName)
 
+    def CheckPython(self) -> None:
+        streamdata, isExist = self.RunCommand("python --version")
+
+        if isExist:
+            self.logger.info(f"Using {streamdata}")
+        else:
+            self.logger.error("Python is not exists")
+            exit(1)
+
+    def SetPaths(self, envName : str) -> None:
+        if exists("paths.json"):
+            with open('paths.json', "r") as pathFile:
+                self.paths = load(pathFile)
+        else:
+            winPath = f"{envName}\\Scripts"
+
+            if self.opsys == "Windows":
+                self.paths["python"] = f"{winPath}\\python.exe"
+                self.paths["pip"] = f"{winPath}\\pip.exe"
+                self.paths["activate"] = f"{winPath}\\activate.bat"
+                self.paths["deactivate"] = f"{winPath}\\deactivate.bat"
+            elif self.opsys == "Linux":
+                self.paths["activate"] = f"source {envName}/bin/activate"
+                self.paths["python"] = f"{self.paths['activate']} && python3"
+                self.paths["pip"] = f"{self.paths['activate']} && pip3"
+                self.paths["deactivate"] = ""
+
+    def SavePaths(self) -> None:
+        with open("paths.json", "w") as file:
+            jsonObject = dumps(self.paths, indent=1)
+            file.write(jsonObject)
+
     """
-    Get Command Functions
+    Installation Functions
     """
 
-    def GetCreateEnvironmentCommand(self):
-        venvCommand = ""
-        if self.osName == 'Linux':
-            venvCommand += f"pip install virtualenv && virtual {self.venvName}"
-        elif self.osName == 'Windows':
-            venvCommand += f"python -m venv {self.venvName}"
+    def CreateRequirementsFile(self) -> None:
+        command = f"{self.paths['pip']} freeze > requirements.txt"
+        self.CheckCommand(self.RunCommand(command), "requirements file is updated", "Cant create requirements file")
+
+    def CheckPackages(self, filename : str) -> tuple:
+        self.logger.info("Checking Packages")
+        command = f"{self.paths['pip']} freeze > temp_requirements.txt"
+        self.RunCommand(command)
+
+        envPackages = []
+        with open("temp_requirements.txt", "r") as envFile:
+            envPackages = set(envFile.readlines())
+
+        remove("temp_requirements.txt")
+
+        wantedPackages = []
+        with open(filename, "r") as wantedFile:
+            wantedPackages = set(wantedFile.readlines())
+
+        notIncludedPackages = wantedPackages - envPackages
+
+        return (notIncludedPackages, wantedPackages.issubset(envPackages))
+
+    def InstallWRequirements(self, filename : str="requirements.txt") -> None:
+        needToDownload, isContains = self.CheckPackages(filename)
+        if isContains:
+            self.logger.info("Requirements are already installed")
+            return
+
+        for package in needToDownload:
+            package = package.strip()
+            command = f"{self.paths['pip']} install {package}"
+            self.logger.info(f"Installing {package}")
+            self.RunCommand(command)
+
+        self.CreateRequirementsFile()
+
+    """
+    Git Functions
+    """
+    def CheckGit(self) -> bool:
+        streamdata, isExist = self.RunCommand("git --version")
         
-        return venvCommand
-    
-    def GetInstallWRequirementsCommand(self):
-        venvCommand = ""
-        if self.osName == 'Linux':
-            venvCommand += f"source {self.venvName}/bin/activate && "
-            venvCommand += "pip3 install -r requirements.txt && "
-            venvCommand += "deactivate"
-        elif self.osName == 'Windows':
-            venvCommand += f".\\{self.venvName}\\Scripts\\activate.bat && "
-            venvCommand += f".\\{self.venvName}\\Scripts\\pip.exe install -r requirements.txt && "
-            venvCommand += f".\\{self.venvName}\\Scripts\\deactivate.bat"
-        
-        return venvCommand
-    
-    def GetInstallWPackagesCommand(self, path):
-        packageFile = open(path, "r")
-        packages = packageFile.readlines()
-        packageFile.close()
+        if isExist:
+            self.logger.info(f"Using {streamdata}")
+        else:
+            self.logger.error("Cant found git")
 
-        pipPackageCommand = "".join(packages).replace("\n", " ")
+        return isExist
 
-        venvCommand = ""
-        if self.osName == 'Linux':
-            venvCommand += f"source {self.venvName}/bin/activate && "
-            venvCommand += f"pip3 install {pipPackageCommand} && "
-            venvCommand += "deactivate"
-        elif self.osName == 'Windows':
-            venvCommand += f".\\{self.venvName}\\Scripts\\activate.bat && "
-            venvCommand += f".\\{self.venvName}\\Scripts\\pip.exe install {pipPackageCommand} && "
-            venvCommand += f".\\{self.venvName}\\Scripts\\deactivate.bat"
-        
-        return venvCommand
-    
-    def GetCreateRequirementsFileCommand(self):
-        venvCommand = ""
-        if self.osName == 'Linux':
-            venvCommand += f"source {self.venvName}/bin/activate && "
-            venvCommand += f"pip3 freeze > requirements.txt && "
-            venvCommand += "deactivate"
-        elif self.osName == 'Windows':
-            venvCommand += f".\\{self.venvName}\\Scripts\\activate.bat && "
-            venvCommand += f".\\{self.venvName}\\Scripts\\pip.exe freeze > requirements.txt && "
-            venvCommand += f".\\{self.venvName}\\Scripts\\deactivate.bat"
+    def GetRepositoryName(self, repoLink : str) -> str:
+        regexPattern = "([^/]+)\\.git$"
+        pattern = compile(regexPattern)
+        matcher = pattern.search(repoLink)
+        return matcher.group(1)
 
-        return venvCommand
-    
-    def GetCreateNewRequirementsFileCommand(self):
-        venvCommand = ""
-        if self.osName == 'Linux':
-            venvCommand += f"source {self.venvName}/bin/activate && pip3 freeze > new_requirements.txt"
-        elif self.osName == 'Windows':
-            venvCommand += f".\\{self.venvName}\\Scripts\\activate.bat && .\\{self.venvName}\\Scripts\\pip.exe freeze > new_requirements.txt"
-
-        return venvCommand
-    
-    def GetInstallWNewRequirementsFileCommand(self):
-        venvCommand = ""
-        if self.osName == 'Linux':
-            venvCommand += "pip3 install -r new_requirements.txt --upgrade && "
-            venvCommand += "deactivate"
-        elif self.osName == 'Windows':
-            venvCommand += f".\\{self.venvName}\\Scripts\\pip.exe install -r new_requirements.txt --upgrade && "
-            venvCommand += f".\\{self.venvName}\\Scripts\\deactivate.bat"
-        
-        return venvCommand
-    
-    def GetRunFileCommand(self, filepath):
-        venvCommand = ""
-        if self.osName == 'Linux':
-            venvCommand += f"source {self.venvName}/bin/activate && "
-            venvCommand += f"python3 {filepath} && "
-            venvCommand += "deactivate"
-        elif self.osName == 'Windows':
-            venvCommand += f".\\{self.venvName}\\Scripts\\activate.bat && "
-            venvCommand += f".\\{self.venvName}\\Scripts\\python.exe {filepath} && "
-            venvCommand += f".\\{self.venvName}\\Scripts\\deactivate.bat"
-
-        return venvCommand
-    
-    def GetRunCommandInEnvironmentCommand(self, command):
-        venvCommand = ""
-        if self.osName == 'Linux':
-            venvCommand += f"source {self.venvName}/bin/activate && "
-            venvCommand += f"{command} && "
-            venvCommand += "deactivate"
-        elif self.osName == 'Windows':
-            venvCommand += f".\\{self.venvName}\\Scripts\\activate.bat && "
-            venvCommand += f"{command} && "
-            venvCommand += f".\\{self.venvName}\\Scripts\\deactivate.bat"
-
-        return venvCommand
-    
-    """
-    Run Functions
-    """
-
-    def RunFile(self, filepath):
-        if exists(self.venvName) is False:
-            print(f"{self.venvName} is not exists")
-            return
-        
-        if exists(filepath) is False:
-            print(f"{filepath} is not exists")
+    def CloneRepository(self, repoLink : str, repoKey : str, parentFolder : str="") -> None:
+        if self.CheckGit() is False:
+            self.logger.error("Cannot detect git")
             return
 
-        self.RunCommand(self.GetRunFileCommand(filepath))
+        repoName = self.GetRepositoryName(repoLink)
+        realRepoKey = repoKey + "-repo"
 
-    def RunCommandInEnvironment(self, command):
-        if exists(self.venvName) is False:
-            print(f"{self.venvName} is not exists")
+        if realRepoKey in self.paths.keys():
+            self.logger.info(f"{repoName} is exists")
             return
+
+        self.paths[realRepoKey] = join(parentFolder, repoName)
+        self.SavePaths()
+
+        if exists(join(self.currentDir, parentFolder)) is False:
+            mkdir(join(self.currentDir, parentFolder))
+
+        if parentFolder == "":
+            command = f"git clone --recursive {repoLink}"
+        else:
+            command = f"cd {parentFolder} && git clone --recursive {repoLink} && cd {self.currentDir}"
         
-        self.RunCommand(self.GetRunCommandInEnvironmentCommand(command))
+        self.logger.info(f"Cloning {repoLink}")
+        self.RunCommand(command)
+
+    def UpdateRepository(self, repoKey : str) -> None:
+        if self.CheckGit() is False:
+            self.logger.error("Cannot detect git")
+            return
+
+        realRepoKey = repoKey + "-repo"
+
+        if realRepoKey not in self.paths.keys():
+            self.logger.info(f"{repoKey} is not exists")
+            return
+
+        command = f"cd {self.paths[realRepoKey]} && git pull && cd {self.currentDir}"
+        
+        self.logger.info(f"Updating {repoKey}")
+        self.RunCommand(command)
+
+    def UpdateAllRepositories(self) -> None:
+        if self.CheckGit() is False:
+            self.logger.error("Cannot detect git")
+            return
+
+        for key in self.paths.keys():
+            if "-repo" not in key:
+                continue
+            
+            self.UpdateRepository(key[:-5])
+
+    def RunScriptInsideRepository(self, repoKey : str, repoCommand : str) -> None:
+        realRepoKey = repoKey + "-repo"
+
+        if realRepoKey not in self.paths.keys():
+            self.logger.info(f"{repoKey} is not exists")
+            return
+
+        command = ""
+        if self.opsys == "Windows":
+            command = f"cd {self.paths[realRepoKey]} && {join(self.currentDir, self.paths['python'])} {repoCommand} && cd {self.currentDir}"
+        elif self.opsys == "Linux":
+            command = f"{self.paths['activate']} && cd {self.paths[realRepoKey]} && python3 {repoCommand} && cd {self.currentDir}"
+
+        self.logger.info(f"Running {repoCommand}")
+        self.RunCommand(command)
+
+    def InstallRequirementsFromRepository(self, repoKey : str, file : str = "requirements.txt") -> None:
+        realRepoKey = repoKey + "-repo"
+
+        if realRepoKey not in self.paths.keys():
+            self.logger.info(f"{repoKey} is not exists")
+            return
+
+        self.InstallWRequirements(join(self.paths[realRepoKey], file))
+
+    def InstallRequirementsFromAllRepositories(self) -> None:
+        if self.CheckGit() is False:
+            self.logger.error("Cannot detect git")
+            return
+
+        for key in self.paths.keys():
+            if "-repo" not in key:
+                continue
+            
+            self.InstallRequirementsFromRepository(key[:-5])
 
     """
-    Create Functions
+    Running Functions
     """
 
-    def CreateEnvironment(self, removeExistsEnvironment=False):
-        if removeExistsEnvironment is False and exists(self.venvName):
-            print(f"{self.venvName} virtual environment is already exists")
-            return
+    def CheckCommand(self, terminalData : tuple, successMsg : str, failureMsg : str) -> None:
+        _, isSuccess = terminalData
+
+        if isSuccess:
+            self.logger.info(successMsg)
+        else:
+            self.logger.error(failureMsg)
+            self.logger.error("ERROR OCCURED!!! Check venvLog file")
+            exit(1)
+
+    def RunScript(self, filename : str, args : str="") -> None:
+        self.logger.info(f"Running {filename}.py with [{', '.join(args.split(' '))}] args")
+        command = f"{self.paths['python']} {filename}.py {args}"
+
+        process = run(command, shell=True, check=True)
+
+        if process.returncode == 0:
+            self.logger.info(f"{filename}.py run successfully")
+        else:
+            self.logger.error(f"Cant run {filename}.py file")
+
+    def RunCommand(self, command : str, isCheck : bool = True) -> tuple:
+        process = run(command, shell=True, check=isCheck, capture_output=True)
         
-        if exists(self.venvName):
-            rmtree(self.venvName)
+        streamdata = process.stdout
+        streamdata = streamdata.decode('UTF-8')
+        streamdata = streamdata.strip()
 
-        self.RunCommand(self.GetCreateEnvironmentCommand())
+        with open("venvLog.txt", "a") as file:
+            if streamdata != "":
+                file.write(f"{'-'*200}\n")
+                file.write(f"Command : {command}\n")
+                file.write(f"Output  :\n{streamdata}\n")
 
-    def CreateEnvironmentWRequirementsFile(self, removeExistsEnvironment=False):
-        self.CreateEnvironment(removeExistsEnvironment)
-        self.InstallWRequirementsFile()
-
-    def CreateEnvironmentWPackageNames(self, packagesPath, removeExistsEnvironment=False):
-        self.CreateEnvironment(removeExistsEnvironment)
-        self.InstallWPackageNames(packagesPath)
-
-    """
-    Install Functions
-    """
-
-    def InstallWRequirementsFile(self):
-        if exists(self.venvName) is False:
-            print(f"{self.venvName} is not exists")
-            return
-
-        if exists("requirements.txt") is False:
-            print("requirements.txt is not found")
-            return
-
-        self.RunCommand(self.GetInstallWRequirementsCommand())
-
-    def InstallWPackageName(self, packageName):
-        if exists(self.venvName) is False:
-            print(f"{self.venvName} is not exists")
-            return
-        
-        filepath = "single_package.txt"
-
-        file = open(filepath, "w")
-        file.write(packageName)
-        file.close()
-
-        self.InstallWPackageNames(filepath)
-
-        remove(filepath)
-
-    def InstallWPackageNames(self, path):
-        if exists(self.venvName) is False:
-            print(f"{self.venvName} is not exists")
-            return
-
-        if exists(path) is False:
-            print(f"{path} is not exists")
-            return
-
-        self.RunCommand(self.GetInstallWPackagesCommand(path))
-        self.RunCommand(self.GetCreateRequirementsFileCommand())
-
-    """
-    Update Functions
-    """
-    def UpdateEnvironment(self):
-        if exists(self.venvName) is False:
-            print(f"{self.venvName} is not exists")
-            return
-
-        print("Updating ...")
-        self.RunCommand(self.GetCreateNewRequirementsFileCommand())
-
-        sleep(0.5)
-
-        file = open("new_requirements.txt", "r")
-        lines = file.readlines()
-        file.close()
-
-        file = open("new_requirements.txt", "w")
-        for line in lines:
-            file.writelines(line.replace("==", ">="))
-
-        file.close()
-        
-        sleep(0.5)
-
-        self.RunCommand(self.GetInstallWNewRequirementsFileCommand())
-        remove("new_requirements.txt")
+        return (streamdata, True) if process.returncode == 0 else (streamdata, False)
