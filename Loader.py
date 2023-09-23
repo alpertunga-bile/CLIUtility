58279aa925e48c56c5df4ff6c857be13570c1b4f
alpertunga-bile <76731692+alpertunga-bile@users.noreply.github.com>
2023-09-23 13:51:50 +0300

    Loader class is updated

--- Loader.py
+++ Loader.py
@@ -1,44 +1,36 @@
-import sys
+from datetime import datetime
 from collections.abc import KeysView, ItemsView
 
-class Loader:
-    desc = ""
-    start = -1
-    end = -1
-    step = 1
-    counter = 0
-    loadChar = '#'
-    toAdd = 0
-    iteration = None
 
-    def SetValuesForRangeObject(self, givenRange : range, description : str, loadingChar : str):
+class Loader:
+    desc: str = ""
+    start: int = -1
+    end: int = -1
+    step: int = 1
+    counter: int = 0
+    loadChar: str = "#"
+    toAdd: int = 0
+    iteration = None
+    start_time = None
+    end_time = None
+    percentage = 0
+
+    def SetValuesForRangeObject(
+        self, givenRange: range, description: str, loadingChar: str
+    ):
         if givenRange.start < 0 and givenRange.stop > 0:
             self.toAdd = -1 * givenRange.start
         elif givenRange.start < 0 and givenRange.stop < 0:
             self.toAdd = -1 * min(givenRange.start, givenRange.stop)
-    
+
         self.iteration = iter(givenRange)
-        self.SetAttributes(givenRange.start, givenRange.stop, givenRange.step, description, loadingChar)
+        self.SetAttributes(
+            givenRange.start, givenRange.stop, givenRange.step, description, loadingChar
+        )
 
-    def SetValuesForListTupleSetObjects(self, givenList : list, description : str, loadingChar : str):
-        self.iteration = iter(givenList)
-        self.SetAttributes(0, len(givenList), 1, description, loadingChar)
-
-    def __init__(self, iterableObject, description="Loader", loadingChar="#"):
-        objectType = type(iterableObject)
-        
-        if objectType is range:
-            self.SetValuesForRangeObject(iterableObject, description, loadingChar)
-        elif objectType is list or objectType is tuple or objectType is set:
-            self.SetValuesForListTupleSetObjects(iterableObject, description, loadingChar)
-        elif isinstance(iterableObject, KeysView) or isinstance(iterableObject, ItemsView):
-            tempList = list(iterableObject)
-            self.SetValuesForListTupleSetObjects(tempList, description, loadingChar)
-        else:
-            print(f"{objectType} for iterableObjects is not supported")
-            exit(0)
-
-    def SetAttributes(self, start : int, end : int, step : int, description : str, loadingChar : str):
+    def SetAttributes(
+        self, start: int, end: int, step: int, description: str, loadingChar: str
+    ):
         self.start = start
         self.end = end - 1
         self.step = step
@@ -46,28 +38,72 @@ class Loader:
         self.desc = description
         self.loadChar = loadingChar
 
+    def SetValuesForListTupleSetObjects(
+        self, givenList: list, description: str, loadingChar: str
+    ) -> None:
+        self.iteration = iter(givenList)
+        self.SetAttributes(0, len(givenList), 1, description, loadingChar)
+
+    def __init__(self, iterableObject, description="Loader", loadingChar="#"):
+        objectType = type(iterableObject)
+
+        if objectType is range:
+            self.SetValuesForRangeObject(iterableObject, description, loadingChar)
+        elif objectType is list or objectType is tuple or objectType is set:
+            self.SetValuesForListTupleSetObjects(
+                iterableObject, description, loadingChar
+            )
+        elif isinstance(iterableObject, KeysView) or isinstance(
+            iterableObject, ItemsView
+        ):
+            tempList = list(iterableObject)
+            self.SetValuesForListTupleSetObjects(tempList, description, loadingChar)
+        else:
+            print(f"{objectType} for iterableObjects is not supported")
+            exit(0)
+
     def __iter__(self):
-        self.Update()
+        self.Update(True)
+        self.PrintBar(0.0)
         self.counter = self.start - 1
+        self.start_time = datetime.now()
+        self.end_time = datetime.now()
         return self
-    
-    def RemoveLastLine(self):
-        sys.stdout.write('\x1b[1A') 
-        sys.stdout.write('\x1b[2K')
-    
+
     def __next__(self):
         self.counter = self.counter + self.step
         if self.counter <= self.end:
-            self.RemoveLastLine()
-            self.Update()
+            self.start_time = datetime.now()
+            self.Update(False)
+            self.end_time = datetime.now()
+            self.PrintBar((self.end_time - self.start_time).total_seconds())
             return next(self.iteration)
         else:
+            self.Update(True)
+            self.PrintBar(0.0)
+            print("\n")
             raise StopIteration
+        
+    def PrintBar(self, eta: float) -> None:
+        remains = 100 - self.percentage
 
-    def Update(self):
-        percentage = int((float(self.counter + self.toAdd) / float(self.end + self.toAdd)) * 100.0)
+        remain_seconds = int(eta * self.percentage)
+        remain_minutes = int(remain_seconds / 60.0)
+        remian_hours = int(remain_minutes / 60.0)
+
         loadStr = self.desc + " ["
-        loadStr += self.loadChar * percentage
-        loadStr += " " * (100 - percentage)
-        loadStr += f"] %{percentage}"
-        print(loadStr)
+        loadStr += self.loadChar * self.percentage
+        loadStr += " " * remains
+        loadStr += f"] %{self.percentage} ETA [{remian_hours:3} h|{remain_minutes:3} m|{remain_seconds:3} s]"
+        print(loadStr, end="\r")
+
+
+    def Update(self, empty: bool = False):
+        start_point = float(self.counter + self.toAdd)
+        end_point = float(self.end + self.toAdd if self.end + self.toAdd > 0 else 1.0)
+        percentage = int((start_point / end_point) * 100.0) if end_point != 1.0 else 100
+
+        if empty:
+            percentage = 100
+
+        self.percentage = percentage
